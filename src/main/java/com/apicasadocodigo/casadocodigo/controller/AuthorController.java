9871cb88cc7b067d49167d6c52b69321d7d5eeab
Ronaldo Melo <rpm.melkor@gmail.com>
2020-09-28 19:56:52 -0300

    Author Controller - Repairs

--- src/main/java/com/apicasadocodigo/casadocodigo/controller/AuthorController.java
+++ src/main/java/com/apicasadocodigo/casadocodigo/controller/AuthorController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/author")
+@RequestMapping("/authors")
 public class AuthorController {
 
     @Autowired
