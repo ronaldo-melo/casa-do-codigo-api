d22a446d5fe40dc04ca88cacc11343dc3e19adb0
Ronaldo Melo <rpm.melkor@gmail.com>
2020-09-28 21:54:46 -0300

    AuthorController - save method

--- src/main/java/com/apicasadocodigo/casadocodigo/controller/AuthorController.java
+++ src/main/java/com/apicasadocodigo/casadocodigo/controller/AuthorController.java
@@ -2,11 +2,15 @@ package com.apicasadocodigo.casadocodigo.controller;
 
 import com.apicasadocodigo.casadocodigo.model.Author;
 import com.apicasadocodigo.casadocodigo.repository.AuthorRepository;
+import com.apicasadocodigo.casadocodigo.request.AuthorRequest;
+import com.apicasadocodigo.casadocodigo.util.AuthorRequestToModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,4 +25,13 @@ public class AuthorController {
         return authorRepository.findAll();
     }
 
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody @Valid AuthorRequest requestBody, UriComponentsBuilder builder) {
+
+        Author newAuthor = AuthorRequestToModel.toAuthor(requestBody);
+        authorRepository.save(newAuthor);
+        URI addressConsult = builder.path("/authors/{id}").build(newAuthor.getId());
+
+        return ResponseEntity.created(addressConsult).build();
+    }
 }
